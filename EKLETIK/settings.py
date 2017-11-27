58ad2ec1b3b8d7f990e51a9c3a9880520286b4b5
Leo <csneto@149-162-231-154.dhcp-in.iupui.edu>
2017-11-27 14:24:58 -0500

    -- Edited installed apps

--- EKLETIK/settings.py
+++ EKLETIK/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'requests', 'random', 'json', 'urllib',
     'rest_framework',
     'EKSite',
 ]
