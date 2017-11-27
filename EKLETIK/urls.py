ad57abad0578775f6fe72dde885c2f9e4fe041e5
Leo <oleoneto@users.noreply.github.com>
2017-11-27 04:26:59 -0500

    Revert "APIs moved from .js to .py"

--- EKLETIK/urls.py
+++ EKLETIK/urls.py
@@ -55,8 +55,6 @@ urlpatterns = [
     url(r'^empresa/', site.company, name='company'),
     url(r'^contacto/', site.contact, name='contact'),
     url(r'^message/', site.message, name='message'),
-    url(r'^eventmate/', labs.eventmate),
-    url(r'^news/', labs.news),
 
     # SEARCH
     url(r'^p/', Search.SearchResults, name='searchResults'),
@@ -116,7 +114,6 @@ urlpatterns = [
     url(r'^labs/news', labs.news, name='news'),
     url(r'^labs/jax-audio', labs.jax_audio, name='jax-audio'),
     url(r'^labs/jax', labs.jax, name='jax'),
-    url(r'^labs/eventmate', labs.eventmate, name='eventmate'),
     url(r'^labs/', labs.experimentos, name="labs"),
 
 
