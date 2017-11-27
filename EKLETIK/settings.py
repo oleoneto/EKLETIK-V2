9f629df332c2dbe0bbc0d5381b1118e6278db31c
Leo <csneto@149-162-231-154.dhcp-in.iupui.edu>
2017-11-27 13:16:39 -0500

    -- Provisional fix of rewritten APIs from .js to .py files

--- EKLETIK/settings.py
+++ EKLETIK/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'requests', 'random', 'json', 'urllib',
     'rest_framework',
     'EKSite',
 ]
