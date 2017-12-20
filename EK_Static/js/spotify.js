c50fcef7c625930ef904352af7b536f2a23788b6
Leo <>
2017-12-20 01:45:59 -0500

    Added an event listener to the window in spotify.js

--- EK_Static/js/spotify.js
+++ EK_Static/js/spotify.js
@@ -1,43 +1,63 @@
 /*
-// ekos.css
+// spotify.js
 // EKOS Visual Framework.
 // EKOS is based on Bootstrap 4 and jQuery.
 // Developed by Leo Neto on Dec 18, 2017.
 // Ekletik Studios. Open-source License.
 */
 
+
 var allSongs = $('audio');
 var songIcons = $('button > i');
 var pageTitle = document.getElementsByTagName('title');
 var mainTitle = pageTitle[0].textContent;
+var currentSong;
+var currentButton;
+var currentIcon;
+var currentTitle;
+var songTitle;
 
-// var audio = document.getElementById("song-b3");
+
 
 $(document).ready(function() {
-    console.log("Loaded spotify.js...");
-    // console.log(audio);
-    //ProcessEndedTrack(audio);
+    console.log("Loaded spotify.js");
+
+    window.addEventListener('visibilitychange', function() {
+        if (document.hidden){
+            if (currentSong) {
+                StopTracks();
+                $('title')[0].textContent = "Paused: " + currentTitle;
+            }
+        } else {
+            $('title')[0].textContent = mainTitle;
+            if (currentSong) {
+                ResumePlayBack(currentSong);
+                UpdatePlayIcon(currentIcon);
+            }
+        }
+    });
+
 });
 
 
 function PlayTrack(id) {
-    var currentSong = document.getElementById("song-" + id);
-    var currentButton = document.getElementById(id);
-    var currentIcon = currentButton.children[0];
-    var currentTitle = document.getElementById("title-"+id);
+    currentSong = document.getElementById("song-" + id);
+    currentButton = document.getElementById(id);
+    currentIcon = currentButton.children[0];
+    currentTitle = document.getElementById("title-"+id);
     currentTitle = currentTitle.textContent;
-    var title = "Now Playing: " + currentTitle;
+    songTitle = "Playing: " + currentTitle;
 
     // console.log(currentTitle);
 
 
     if (currentSong.paused === true) {
         currentSong.play();
-        currentIcon.className = "fa fa-circle-o-notch fa-spin";
-        UpdatePageTitle(title);
+        UpdatePlayIcon(currentIcon);
+        UpdatePageTitle(songTitle);
     } else {
         currentSong.pause();
-        currentIcon.className = "fa fa-play";
+        UpdatePlayIcon(currentIcon);
     }
 
     StopTracks(currentSong, currentIcon, id);
@@ -58,16 +78,28 @@ function StopTracks(currentSong, currentIcon, id) {
     }
 }
 
-
 function ProcessEndedTrack(el) {
     var currentButton = el.parentElement;
     var currentIcon = currentButton.children[0];
     currentIcon.className = "fa fa-check";
+    currentSong = null;
     UpdatePageTitle(mainTitle);
-    // console.log(el.parentElement);
 }
 
 function UpdatePageTitle(title) {
   pageTitle[0].textContent = title;
   // console.log(pageTitle[0].textContent);
+}
+
+function UpdatePlayIcon(playIcon) {
+    if (playIcon.className == 'fa fa-play'){
+        playIcon.className = 'fa fa-circle-o-notch fa-spin';
+    } else if (playIcon.className == 'fa fa-circle-o-notch fa-spin'){
+        playIcon.className = 'fa fa-play';
+    }
+}
+
+function ResumePlayBack(currentSong) {
+    currentSong.play();
+    UpdatePageTitle(songTitle);
 }
